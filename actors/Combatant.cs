77f7bfae637e7d901fbb026459a096220b047f8a
Quadtree <quadtree@gmail.com>
2021-10-01 21:13:18 -0700

    Updated

--- actors/Combatant.cs
+++ actors/Combatant.cs
@@ -26,17 +26,19 @@ public class Combatant : Spatial
         if (MoveLeft && !MoveRight) targetSpeed = -1f;
         if (!MoveLeft && MoveRight) targetSpeed = 1f;
 
-        /*
         var wheelJoint = this.FindChildByType<HingeJoint>();
         wheelJoint.Motor__enable = true;
         wheelJoint.Motor__maxImpulse = 1000f;
         wheelJoint.Motor__targetVelocity = -targetSpeed * 4;
-        */
 
+
+        /*
         var wheels = this.FindChildByName<RigidBody>("Wheels");
         var body = this.FindChildByName<RigidBody>("Body");
 
         wheels.ApplyTorqueImpulse(new Vector3(0, 0, targetSpeed * -0.05f));
+        body.ApplyImpulse(wheels.GetGlobalLocation() - body.GetGlobalLocation(), new Vector3(targetSpeed * 1f, 0, 0));
+        */
     }
 
     public override void _Input(InputEvent @event)
