ef96db5ea22d178bc037f6b96a87ab42e70559cf
Quadtree <quadtree@gmail.com>
2021-10-01 22:44:19 -0700

    Updated

--- actors/Combatant.cs
+++ actors/Combatant.cs
@@ -57,7 +57,7 @@ public class Combatant : Spatial
 
             //Console.WriteLine(angle.origin);
 
-            var angle = -Mathf.Atan2(pos.y - armRootLocation.origin.y, pos.x - armRootLocation.origin.x) * (180 / Mathf.Pi) + 90;
+            var angle = -Mathf.Atan2(pos.y - armRootLocation.origin.y, pos.x - armRootLocation.origin.x) * (180 / Mathf.Pi) + 180;
             var dist = new Vector3(pos.x, pos.y, 0).DistanceTo(new Vector3(armRootLocation.origin.x, armRootLocation.origin.y, 0));
 
             dist = Mathf.Min(dist, 2);
@@ -69,8 +69,8 @@ public class Combatant : Spatial
             //Console.WriteLine($"armRootLocation={armRootLocation} pos={pos}");
             Console.WriteLine($"angle={angle} dist={dist} od={od}");
 
-            //armJoint.AngularLimitZ__lowerAngle = angle - 0.05f;
-            //armJoint.AngularLimitZ__upperAngle = angle + 0.05f;
+            armJoint.AngularLimitZ__lowerAngle = angle - 0.05f;
+            armJoint.AngularLimitZ__upperAngle = angle + 0.05f;
 
             //armJoint.LinearLimitY__lowerDistance = dist - 0.01f;
             //armJoint.LinearLimitY__upperDistance = dist + 0.01f;
